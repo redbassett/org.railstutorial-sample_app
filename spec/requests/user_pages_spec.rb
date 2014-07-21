738e4a83b5bf3ecdfb082605c62403dfe7ce8d5a
Harry Grillo <redbassett@gmail.com>
2014-07-21 03:45:45 -0500

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,12 +2,45 @@ require 'spec_helper'
 require 'support/utilities'
 
 describe "User pages" do
-	subject { page }
+  subject { page }
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-	describe "signup page" do
-		before { visit signup_path }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
-		it { should have_content('Sign up') }
-		it { should have_title(full_title('Sign Up')) }
-	end
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign Up')) }
+  end
+  
+  describe "signup" do
+    before { visit signup_path }
+    
+    let(:submit) { "Create my account" }
+    
+    describe "with invalid information" do
+      it "should not creat a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      
+      it "shou;d create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
