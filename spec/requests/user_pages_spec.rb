9efb4236207b723591ad8d453a8f4fb975db6d97
Harry Grillo <redbassett@gmail.com>
2014-07-21 04:24:12 -0500

    Fixes typo in user tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,7 +38,7 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
       
-      it "shou;d create a user" do
+      it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
