ee286ddbd52f86760eedc1e8b7ba1c7906b06f3d
Harry Grillo <redbassett@gmail.com>
2014-07-21 04:40:47 -0500

    Fixes minor syntax spacing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
 	#Returns Gravatar for given user.
-	def gravatar_for(user, options = {size: 50 })
+	def gravatar_for(user, options = { size: 50 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
