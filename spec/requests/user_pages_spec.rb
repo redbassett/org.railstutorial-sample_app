9424907a37f4fdc78815211b36fd4b86cbed4bf5
Harry Grillo <redbassett@gmail.com>
2014-07-23 16:02:03 -0500

    Finished branch

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,14 +15,14 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
+    it { should have_content('Sign Up') }
     it { should have_title(full_title('Sign Up')) }
   end
   
   describe "signup" do
     before { visit signup_path }
     
-    let(:submit) { "Create my account" }
+    let(:submit) { "Create My Account" }
     
     describe "with invalid information" do
       it "should not creat a user" do
@@ -53,6 +53,15 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
         
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+        
+        it { should have_link('Sign Out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
